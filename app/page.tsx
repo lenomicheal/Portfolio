1cd4162f481fbddf7111fb7ed89105b1e4a01133
lenomicheal <lenomicheal8@gmail.com>
2026-02-05 07:41:31 +0530

    Shadcn components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
-const Page = () => {
+const page = () => {
   return (
-    <div>
-      Hello
+    <div className='text-3xl bg-red-700 h-screen w-screen flex justify-center items-center'> 
+      <Button variant="secondary" size="lg">Fuck You</Button>
     </div>
   )
 }
 
-export default Page
+export default page
